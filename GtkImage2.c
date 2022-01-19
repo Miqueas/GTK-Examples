fc80b89fc9db746962cbf8402f597276aef2b338
Miqueas <miqueas2020@yahoo.com>
2022-01-19 17:58:44 +0000

    Added another GtkImage example

---
+++ GtkImage2.c
@@ -0,0 +1,57 @@
+#include <gtk/gtk.h>
+
+void app_activate(GApplication *self, gpointer data);
+void app_startup(GApplication *self, gpointer data);
+
+int main(int argc, char **argv) {
+  GtkApplication *app = g_object_new(
+    GTK_TYPE_APPLICATION,
+    "application-id", "com.github.Miqueas.C-GTK3-Examples.GtkImage2",
+    "flags", G_APPLICATION_FLAGS_NONE,
+    NULL
+  );
+
+  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+
+  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return res;
+}
+
+void app_activate(GApplication *self, gpointer data) {
+  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
+
+  GtkWidget *img = g_object_new(
+    GTK_TYPE_IMAGE,
+    "visible", TRUE,
+    "icon-name", "computer",
+    "pixel-size", 256,
+    NULL
+  );
+
+  gtk_container_add(GTK_CONTAINER(win), img);
+  gtk_window_present(win);
+}
+
+void app_startup(GApplication *self, gpointer data) {
+  GtkWidget *win = g_object_new(
+    GTK_TYPE_APPLICATION_WINDOW,
+    "application", self,
+    "default-width", 400,
+    "default-height", 400,
+    NULL
+  );
+
+  GtkWidget *header = g_object_new(
+    GTK_TYPE_HEADER_BAR,
+    "visible", TRUE,
+    "show-close-button", TRUE,
+    "title", "My App",
+    "subtitle", "An awesome app that you'll love",
+    NULL
+  );
+
+  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+}
