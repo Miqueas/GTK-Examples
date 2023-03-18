258e71a52719a70d54009541fc65429652d1b8e5
Miqueas <miqueas2020@yahoo.com>
2023-03-17 23:05:38 -0500

    Lua: added gtk 4 application example
    And updated gtk 3 application example

--- lua/gtk4/application.lua
+++ lua/gtk4/application.lua
@@ -3,7 +3,7 @@ local Gtk = lgi.require("Gtk", "3.0")
 
 -- GtkApplication: Provides an application interface
 
-local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Application"
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk4.Application"
 local app = Gtk.Application({ application_id = appID })
 
 -- I recommend to check out this links for more information:
