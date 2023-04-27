efe245ee9d23d299e1f33b9f09fde4616b2c32f5
Miqueas <miqueas2020@yahoo.com>
2023-04-27 18:15:05 -0500

    Lua: updated gtk 3 expander example

--- lua/gtk3/expander.lua
+++ lua/gtk3/expander.lua
@@ -1,28 +1,22 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Expander"
-})
+-- GtkExpander: a "more details" widget
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Expander"
+local appTitle = "GtkExpander"
+local app = Gtk.Application({ application_id = appID })
+
+function app:on_startup()
+  local win = Gtk.ApplicationWindow({
+    title = appTitle,
     application = self,
     default_width = 400,
     border_width = 10,
     resizable = false
   })
-end
 
-function App:on_activate()
-  self.active_window:set_titlebar(Gtk.HeaderBar({
-    visible = true,
-    show_close_button = true,
-    title = "Your app title",
-    subtitle = "Your app subtitle"
-  }))
-
-  local LoremIpsum = [[Duis in metus eros. Duis faucibus rutrum eros eu vestibulum.
+  local loremText = [[Duis in metus eros. Duis faucibus rutrum eros eu vestibulum.
 Proin et arcu nulla. Etiam at lacinia nibh. Vivamus pellentesque nunc nibh,
 ac dignissim massa lobortis ut. Integer eu felis in elit semper ullamcorper
 at in ipsum. Suspendisse tempus massa vel nibh tristique vestibulum.
@@ -30,24 +24,20 @@ Vestibulum varius eu nunc eu interdum. Curabitur pulvinar velit in purus
 facilisis, et auctor augue consequat. Donec finibus felis ligula, a convallis
 justo tristique a.]]
 
-  --[[ GtkExpander:
-
-    A widget that allows hide its child, like a GtkRevealer, but you don't need to
-    show/hide the child programmatically and has a small "hint" that there is
-    "something" hidden.
-
-  ]]
-  local Expand = Gtk.Expander({
+  local expander = Gtk.Expander({
     visible = true,
     label = [[  <span size="large" font_weight="bold">Lorem ipsum</span>  ]],
     use_markup = true,
     resize_toplevel = true,
 
-    Gtk.Label({ visible = true, label = LoremIpsum, wrap = true })
+    Gtk.Label({ visible = true, label = loremText, wrap = true })
   })
 
-  self.active_window:add(Expand)
+  win:add(expander)
+end
+
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
