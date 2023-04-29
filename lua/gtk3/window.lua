b759e38834663db47fa478712e2485261fdc3008
Miqueas <miqueas2020@yahoo.com>
2023-04-29 17:42:41 -0500

    Lua: updated gtk 3 window example

--- lua/gtk3/window.lua
+++ lua/gtk3/window.lua
@@ -1,19 +1,15 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
---[[ GtkWindow:
+-- GtkWindow: a window widget
 
-  Well... A window lmao
-
-]]
-local Window = Gtk.Window({
+local window = Gtk.Window({
   default_width = 400,
   default_height = 400
 })
 
--- The "destroy" signal is emited when the user clicks in the
--- close button
-Window.on_destroy = Gtk.main_quit
-Window:present()
+-- The "destroy" signal is emited when the user clicks the close button
+window.on_destroy = Gtk.main_quit
+window:present()
 
 Gtk.main()
