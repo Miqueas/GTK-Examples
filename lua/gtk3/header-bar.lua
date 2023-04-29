aacc0f875e3ee82e62082044b951753087086c94
Miqueas <miqueas2020@yahoo.com>
2023-04-29 17:38:38 -0500

    Lua: updated gtk 3 header bar example

--- lua/gtk3/header-bar.lua
+++ lua/gtk3/header-bar.lua
@@ -1,40 +1,34 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.HeaderBar"
-})
+-- GtkHeaderBar: a (client-side) title bar decoration widget
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.HeaderBar"
+local appTitle = "GtkHeaderBar"
+local appSubtitle = "A (client-side) title bar decoration widget"
+local app = Gtk.Application({ application_id = appID })
+
+function app:on_startup()
+  local win = Gtk.ApplicationWindow({
     application = self,
     default_width = 400,
     default_height = 400
   })
-end
 
-function App:on_activate()
-  --[[ GtkHeaderBar:
-
-    A container that acts as the title bar of the window.
-    This is a CSD (Client Side Decorations), since we are
-    setting up our own title bar for our window, rather
-    than leaving it up to the WM to decorate it.
-
-    In other examples we talk a little more about CSD using CSS.
-
-  ]]
-  self.active_window:set_titlebar(Gtk.HeaderBar({
-    -- By default, all GTK widgets are not visibles
+  local headerBar = Gtk.HeaderBar({
     visible = true,
     -- GtkHeaderBar don't sets the window controls buttons by default
     -- this property changes that behavior
     show_close_button = true,
-    title = "Your app title",
-    subtitle = "Your app subtitle"
-  }))
+    title = appTitle,
+    subtitle = appSubtitle
+  })
 
+  win:set_titlebar(headerBar)
+end
+
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
