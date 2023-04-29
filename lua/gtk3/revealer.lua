7747a663ea67a9d5c170bb61ab0d8e2c340e3f6d
Miqueas <miqueas2020@yahoo.com>
2023-04-29 17:41:52 -0500

    Lua: updated gtk 3 revealer example

--- lua/gtk3/revealer.lua
+++ lua/gtk3/revealer.lua
@@ -1,51 +1,43 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Revealer"
-})
+-- GtkRevealer: a container for show/hide a widget with an animation.
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Revealer"
+local appTitle = "GtkRevealer"
+local app = Gtk.Application({ application_id = appID })
+
+function app:on_startup()
+  local window = Gtk.ApplicationWindow({
+    title = appTitle,
     application = self,
-    default_width = 400,
-    default_height = 10
-  })
-end
-
-function App:on_activate()
-  local Header = Gtk.HeaderBar({
-    visible = true,
-    show_close_button = true,
-    title = "GtkRevealer"
+    default_width = 200,
+    resizable = false,
+    border_width = 10
   })
 
-  local Btn = Gtk.Button({ visible = true, label = "Toggle message" })
-  Header:pack_start(Btn)
-  self.active_window:set_titlebar(Header)
-
-  local Box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
-
-  --[[ GtkRevealer:
-
-    A container for show/hide a widget with animation.
-
-  ]]
-  local Rev = Gtk.Revealer({
+  local revealer = Gtk.Revealer({
     visible = true,
     reveal_child = true,
 
     Gtk.Label({ visible = true, label = "Hello there!", margin = 10 })
   })
 
-  function Btn:on_clicked()
-    Rev.reveal_child = (not Rev.reveal_child) and true or false
+  local toggleButton = Gtk.ToggleButton({ visible = true, label = "Toggle message", active = revealer.reveal_child })
+
+  function toggleButton:on_clicked()
+    revealer.reveal_child = (not revealer.reveal_child) and true or false
   end
 
-  Box:pack_start(Rev, false, true, 0)
+  local box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
+  box:pack_start(revealer, true, true, 0)
+  box:pack_start(toggleButton, false, false, 0)
 
-  self.active_window:add(Box)
+  window:add(box)
+end
+
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
