4614efeb49e22c849a79a202613bdaec61896d06
Miqueas <miqueas2020@yahoo.com>
2023-04-29 17:41:36 -0500

    Lua: updated gtk 3 paned example

--- lua/gtk3/paned.lua
+++ lua/gtk3/paned.lua
@@ -1,36 +1,29 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Paned"
-})
+-- GtkPaned: a widget with 2 panes and a adjustable division
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Paned"
+local appTitle = "GtkPaned"
+local app = Gtk.Application({ application_id = appID })
+
+function app:on_startup()
+  local window = Gtk.ApplicationWindow({
+    title = appTitle,
     application = self,
     default_width = 400,
     default_height = 400
   })
+
+  local paned = Gtk.Paned({ visible = true, orientation = Gtk.Orientation.HORIZONTAL })
+  paned:pack1(Gtk.Label({ visible = true, label = "Space 1" }), true, false)
+  paned:pack2(Gtk.Label({ visible = true, label = "Space 2" }), true, false)
+
+  window:add(paned)
 end
 
-function App:on_activate()
-  self.active_window:set_titlebar(Gtk.HeaderBar({
-    visible = true,
-    show_close_button = true,
-    title = "GtkPaned"
-  }))
-
-  --[[ GtkPaned:
-
-    A widget with two adjustable panes
-
-  ]]
-  local Paned = Gtk.Paned({ visible = true, orientation = Gtk.Orientation.HORIZONTAL })
-  Paned:pack1(Gtk.Label({ visible = true, label = "Space 1" }), true, false)
-  Paned:pack2(Gtk.Label({ visible = true, label = "Space 2" }), true, false)
-
-  self.active_window:add(Paned)
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
