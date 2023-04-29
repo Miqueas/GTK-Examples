cb2dc7704fc2f5256f5ab12911d21e73161c89df
Miqueas <miqueas2020@yahoo.com>
2023-04-29 17:40:42 -0500

    Lua: updated gtk 3 list box example

--- lua/gtk3/list-box.lua
+++ lua/gtk3/list-box.lua
@@ -1,74 +1,59 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.ListBox1"
-})
+-- GtkListBox: a list container widget
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.ListBox1"
+local appTitle = "GtkListBox"
+local app = Gtk.Application({ application_id = appID })
+
+function app:on_startup()
+  local win = Gtk.ApplicationWindow({
+    title = appTitle,
     application = self,
     default_width = 400,
     default_height = 400,
     border_width = 10
   })
-end
 
-function App:on_activate()
-  self.active_window:set_titlebar(Gtk.HeaderBar({
-    visible = true,
-    show_close_button = true,
-    title = "GtkListBox",
-    subtitle = "Example 1"
-  }))
+  local listBox = Gtk.ListBox({ visible = true })
 
-  local Box = Gtk.Box({
-    visible = true,
-    orientation = Gtk.Orientation.VERTICAL
-  })
-
-  --[[ GtkListBox:
-
-    A container for lists. It works better for lists
-    than a GtkBox because of its vertical nature and
-    the API it offers.
-
-  ]]
-  local List = Gtk.ListBox({ visible = true })
-
-  local Scroll = Gtk.ScrolledWindow({
+  local scrolledWindow = Gtk.ScrolledWindow({
     visible = true,
     shadow_type = Gtk.ShadowType.NONE,
     propagate_natural_width = true,
     propagate_natural_height = true,
 
-    List
+    listBox
   })
 
-  local Btn = Gtk.Button({
+  local button = Gtk.Button({
     visible = true,
     label = "Load",
     halign = Gtk.Align.CENTER,
     valign = Gtk.Align.CENTER
   })
 
-  function Btn:on_clicked()
+  function button:on_clicked()
     for i = 0, 100 do
-      List:insert(Gtk.Label({ visible = true, label = "Text " .. i }), i)
+      listBox:insert(Gtk.Label({ visible = true, label = "Text " .. i }), i)
     end
   end
 
-  local Box = Gtk.Box({
+  local box = Gtk.Box({
     visible = true,
     orientation = Gtk.Orientation.VERTICAL,
     spacing = 10,
 
-    { Scroll, expand = true },
-    { Btn }
+    { scrolledWindow, expand = true },
+    { button }
   })
 
-  self.active_window:add(Box)
+  win:add(box)
+end
+
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
