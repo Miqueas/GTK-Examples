209a918b3da4f2dd20d42d9e3475860c3826f881
Miqueas <miqueas2020@yahoo.com>
2023-04-29 17:39:20 -0500

    Lua: updated gtk 3 info bar example

--- lua/gtk3/info-bar.lua
+++ lua/gtk3/info-bar.lua
@@ -1,70 +1,58 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.InfoBar"
-})
+-- GtkInfoBar: an "in-app notifications" like widget
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.InfoBar"
+local appTitle = "GtkInfoBar"
+local app = Gtk.Application({ application_id = appID })
+
+function app:on_startup()
+  local win = Gtk.ApplicationWindow({
+    title = appTitle,
     application = self,
     default_width = 400,
     default_height = 400,
-    border_width = 10
+    --border_width = 10
   })
-end
 
-function App:on_activate()
-  self.active_window:set_titlebar(Gtk.HeaderBar({
-    visible = true,
-    show_close_button = true,
-    title = "GtkInfoBar"
-  }))
+  local box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
+  local infoButton = Gtk.Button({ visible = true, label = "Info" })
+  local warningButton = Gtk.Button({ visible = true, label = "Warning" })
+  local questionButton = Gtk.Button({ visible = true, label = "Question" })
+  local errorButton = Gtk.Button({ visible = true, label = "Error" })
+  local otherButton = Gtk.Button({ visible = true, label = "Other" })
 
-  local Box         = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
-  local InfoBtn     = Gtk.Button({ visible = true, label = "Info" })
-  local WarningBtn  = Gtk.Button({ visible = true, label = "Warning" })
-  local QuestionBtn = Gtk.Button({ visible = true, label = "Question" })
-  local ErrorBtn    = Gtk.Button({ visible = true, label = "Error" })
-  local OtherBtn    = Gtk.Button({ visible = true, label = "Other" })
-
-  function CreateInfoBar(self, data)
-    --[[ GtkInfoBar:
-
-      A widget that can be used to show messages to the user.
-      Think in it as In-App notifications. The GtkInfoBar API
-      is very similar to the GtkDialog API
-
-    ]]
-    local InfoBar = Gtk.InfoBar({
+  local function createInfoBar(self, data)
+    local infoBar = Gtk.InfoBar({
       visible = true,
       message_type = Gtk.MessageType[self.label:upper()],
       show_close_button = true
     })
 
     -- For this widget, the 'get_content_area()' method always returns a Gtk.Box
-    InfoBar
+    infoBar
       :get_content_area()
       :pack_start(Gtk.Label({ visible = true, label = self.label }), false, true, 0)
 
     -- When an action widget is clicked, this signal is emitted
-    function InfoBar:on_response()
+    function infoBar:on_response()
       -- For this case, just remove it from the Gtk.Box and then destroy it
-      Box:remove(self)
+      box:remove(self)
       self:destroy()
     end
 
     -- Add the info bar to the Gtk.Box
-    Box:pack_start(InfoBar, false, true, 0)
+    box:pack_start(infoBar, false, true, 0)
   end
 
-  InfoBtn.on_clicked     = CreateInfoBar
-  WarningBtn.on_clicked  = CreateInfoBar
-  QuestionBtn.on_clicked = CreateInfoBar
-  ErrorBtn.on_clicked    = CreateInfoBar
-  OtherBtn.on_clicked    = CreateInfoBar
+  infoButton.on_clicked = createInfoBar
+  warningButton.on_clicked = createInfoBar
+  questionButton.on_clicked = createInfoBar
+  errorButton.on_clicked = createInfoBar
+  otherButton.on_clicked = createInfoBar
 
-  local Grid = Gtk.Grid({
+  local grid = Gtk.Grid({
     visible = true,
     column_homogeneous = true,
     row_homogeneous = true,
@@ -74,17 +62,19 @@ function App:on_activate()
     halign = Gtk.Align.CENTER,
     valign = Gtk.Align.CENTER,
 
-    { InfoBtn,     top_attach = 0, left_attach = 0 },
-    { WarningBtn,  top_attach = 0, left_attach = 1 },
-    { QuestionBtn, top_attach = 1, left_attach = 0 },
-    { ErrorBtn,    top_attach = 1, left_attach = 1 },
-    { OtherBtn,    top_attach = 2, left_attach = 0, width = 2 },
+    { infoButton, top_attach = 0, left_attach = 0 },
+    { warningButton, top_attach = 0, left_attach = 1 },
+    { questionButton, top_attach = 1, left_attach = 0 },
+    { errorButton, top_attach = 1, left_attach = 1 },
+    { otherButton, top_attach = 2, left_attach = 0, width = 2 },
   })
 
-  Box:pack_end(Grid, true, true, 0)
+  box:pack_end(grid, true, true, 0)
+  win:add(box)
+end
 
-  self.active_window:add(Box)
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
