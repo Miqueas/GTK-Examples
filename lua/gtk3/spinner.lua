d41b6ede4b9f0d2c51e546f3798fe5cfe9dca15d
Miqueas <miqueas2020@yahoo.com>
2023-04-30 00:34:52 -0500

    Lua: updated gtk 3 spinner example

--- lua/gtk3/spinner.lua
+++ lua/gtk3/spinner.lua
@@ -1,41 +1,23 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local app = Gtk.Application { application_id = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Spinner" }
-
-function app:on_activate()
-  self.active_window:present()
-end
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Spinner"
+local appTitle = "GtkSpinner"
+local app = Gtk.Application({ application_id = appID })
 
 function app:on_startup()
-  local win = Gtk.ApplicationWindow {
+  local window = Gtk.ApplicationWindow({
+    title = appTitle,
     application = self,
     default_width = 300,
     default_height = 300,
     border_width = 10
-  }
-
-  win:set_titlebar(Gtk.HeaderBar {
-    visible = true,
-    show_close_button = true,
-    title = "GtkSpinner"
   })
 
-  local spinner = Gtk.Spinner { visible = true }
-  local btn = Gtk.Button { visible = true, label = "Start" }
+  local spinner = Gtk.Spinner({ visible = true })
+  local button = Gtk.Button({ visible = true, label = "Start" })
 
-  local box = Gtk.Box {
-    visible = true,
-    spacing = 10,
-    orientation = Gtk.Orientation.VERTICAL,
-    valign = Gtk.Align.CENTER,
-    halign = Gtk.Align.CENTER,
-
-    spinner,
-    btn
-  }
-
-  function btn:on_clicked()
+  function button:on_clicked()
     if spinner.active then
       self.label = "Start"
       spinner:stop()
@@ -45,7 +27,22 @@ function app:on_startup()
     end
   end
 
-  win:add(box)
+  local box = Gtk.Box({
+    visible = true,
+    spacing = 10,
+    orientation = Gtk.Orientation.VERTICAL,
+    valign = Gtk.Align.CENTER,
+    halign = Gtk.Align.CENTER,
+
+    spinner,
+    button
+  })
+
+  window:add(box)
+end
+
+function app:on_activate()
+  self.active_window:present()
 end
 
 return app:run(arg)
