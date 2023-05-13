ba6608e58b295deaaaad69001c73cb57d913d054
Miqueas <miqueas2020@yahoo.com>
2023-05-13 10:26:04 -0500

    C: updated gtk 3 application window example

--- c/gtk3/application-window.c
+++ c/gtk3/application-window.c
@@ -1,27 +1,30 @@
 #include <gtk/gtk.h>
 
-void app_startup(GApplication *self, gpointer data);
-void app_activate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.ApplicationWindow";
+const gchar *appTitle = "GtkApplicationWindow";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.ApplicationWindow";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup", G_CALLBACK(app_startup), NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup", G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win = gtk_application_window_new(GTK_APPLICATION(self));
-  gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(self));
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 }
