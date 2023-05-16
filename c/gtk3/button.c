7bda5b3d5a57ea3f2dc2cf524cc8c07384696dc3
Miqueas <miqueas2020@yahoo.com>
2023-05-15 19:25:52 -0500

    C: updated gtk3 button example

--- c/gtk3/button.c
+++ c/gtk3/button.c
@@ -1,42 +1,45 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
-void btn_clicked(GtkButton *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+void onButtonClicked(GtkButton *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.Button";
+const gchar *appTitle = "GtkButton";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.Button";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup", G_CALLBACK(app_startup), NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup", G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win, *btn;
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *button;
 
-  win = gtk_application_window_new(GTK_APPLICATION(self));
-  btn = gtk_button_new_with_label("Click me");
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  button = gtk_button_new_with_label("Click me");
 
-  gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+  gtk_container_add(GTK_CONTAINER(window), button);
 
-  g_object_set(btn, "halign", GTK_ALIGN_CENTER, "valign", GTK_ALIGN_CENTER, NULL);
-
-  g_signal_connect(btn, "clicked", G_CALLBACK(btn_clicked), NULL);
-
-  gtk_container_add(GTK_CONTAINER(win), btn);
+  gtk_widget_set_visible(button, TRUE);
+  gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
+  g_signal_connect(button, "clicked", G_CALLBACK(onButtonClicked), NULL);
 }
 
-void btn_clicked(GtkButton *self, gpointer data) {
+void onButtonClicked(GtkButton *self, gpointer data) {
   g_print("You clicked me!\n");
 }
