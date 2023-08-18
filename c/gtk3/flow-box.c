18ed98c62fa7236e66bc1f95fba77b77c2e3966b
Miqueas <miqueas2020@yahoo.com>
2023-08-17 19:05:12 -0500

    C: refactorizing

--- c/gtk3/flow-box.c
+++ c/gtk3/flow-box.c
@@ -1,14 +1,28 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.FlowBox";
+const gchar *appTitle = "GtkFlowBox";
+const gchar *icons[10] = {
+  "face-angry",
+  "face-surprise",
+  "face-laugh",
+  "face-plain",
+  "face-sad",
+  "face-cool",
+  "face-smirk",
+  "face-sick",
+  "face-kiss",
+  "face-smile"
+};
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.FlowBox";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
   int res = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
@@ -16,70 +30,36 @@ int main(int argc, char **argv) {
   return res;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win = g_object_new(
-    GTK_TYPE_APPLICATION_WINDOW,
-    "application", GTK_APPLICATION(self),
-    "default-width", 400,
-    "default-height", 400,
-    NULL
-  );
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *flowBox, *scrolledWindow;
 
-  GtkWidget *header = g_object_new(
-    GTK_TYPE_HEADER_BAR,
-    "visible", TRUE,
-    "show-close-button", TRUE,
-    "title", "GtkLabel",
-    NULL
-  );
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  flowBox = gtk_flow_box_new();
+  scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
 
-  GtkWidget *flowbox = g_object_new(
-    GTK_TYPE_FLOW_BOX,
-    "visible", TRUE,
-    "valign", GTK_ALIGN_START,
-    "selection-mode", GTK_SELECTION_NONE,
-    "max-children-per-line", 30,
-    NULL
-  );
+  gtk_container_add(GTK_CONTAINER(window), scrolledWindow);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  GtkWidget *scroll = g_object_new(
-    GTK_TYPE_SCROLLED_WINDOW,
-    "visible", TRUE,
-    "hscrollbar-policy", GTK_POLICY_NEVER,
-    NULL
-  );
-
-  char *icons[10] = {
-    "face-angry",
-    "face-surprise",
-    "face-laugh",
-    "face-plain",
-    "face-sad",
-    "face-cool",
-    "face-smirk",
-    "face-sick",
-    "face-kiss",
-    "face-smile"
-  };
+  gtk_flow_box_set_selection_mode(GTK_FLOW_BOX(flowBox), GTK_SELECTION_NONE);
+  gtk_flow_box_set_max_children_per_line(GTK_FLOW_BOX(flowBox), 30);
 
   srand(time(NULL));
 
   for (int i = 0; i < 400; i++) {
-    gtk_flow_box_insert(GTK_FLOW_BOX(flowbox), g_object_new(
-      GTK_TYPE_IMAGE,
-      "visible", TRUE,
-      "icon-name", icons[(rand() % 10)],
-      "pixel-size", 32,
-      NULL
-    ), i);
+    gtk_flow_box_insert(
+      GTK_FLOW_BOX(flowBox),
+      gtk_image_new_from_icon_name(icons[(rand() % 10)], GTK_ICON_SIZE_DIALOG),
+      i
+    );
   }
 
-  gtk_container_add(GTK_CONTAINER(scroll), flowbox);
-  gtk_container_add(GTK_CONTAINER(win), scroll);
-  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+
+  gtk_container_add(GTK_CONTAINER(scrolledWindow), flowBox);
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledWindow), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+  gtk_widget_show_all(scrolledWindow);
 }
