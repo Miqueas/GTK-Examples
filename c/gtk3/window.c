0bf4dd0dc582ed379cd4c81a6e4066c41a30bef5
Miqueas <miqueas2020@yahoo.com>
2023-08-20 11:57:21 -0500

    C: GTK3: refactorized window.c

--- c/gtk3/window.c
+++ c/gtk3/window.c
@@ -1,13 +1,15 @@
 #include <gtk/gtk.h>
 
+const gchar *appTitle = "GtkWindow";
+
 int main(int argc, char **argv) {
   gtk_init(&argc, &argv);
 
-  GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
-
-  g_signal_connect(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-  gtk_window_present(GTK_WINDOW(win));
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_window_present(GTK_WINDOW(window));
   gtk_main();
 
   return 0;
