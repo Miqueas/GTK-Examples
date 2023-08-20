4fa070b05368b5d988fd1c465684d204bbf6671a
Miqueas <miqueas2020@yahoo.com>
2023-08-20 11:58:02 -0500

    C: GTK3: refactorized label.c

--- c/gtk3/label.c
+++ c/gtk3/label.c
@@ -1,50 +1,37 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.Label";
+const gchar *appTitle = "GtkLabel";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.Label";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win = g_object_new(
-    GTK_TYPE_APPLICATION_WINDOW,
-    "application", GTK_APPLICATION(self),
-    "default-width", 400,
-    "default-height", 400,
-    NULL
-  );
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *label;
 
-  GtkWidget *header = g_object_new(
-    GTK_TYPE_HEADER_BAR,
-    "visible", TRUE,
-    "show-close-button", TRUE,
-    "title", "GtkLabel",
-    NULL
-  );
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  label = gtk_label_new("Hello, world!");
 
-  GtkWidget *label = g_object_new(
-    GTK_TYPE_LABEL,
-    "visible", TRUE,
-    "label", "Hello, world!",
-    NULL
-  );
+  gtk_container_add(GTK_CONTAINER(window), label);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  gtk_container_add(GTK_CONTAINER(win), label);
-  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+  gtk_widget_set_visible(label, TRUE);
 }
