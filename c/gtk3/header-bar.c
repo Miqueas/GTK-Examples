130c95e234876914ff169afc18dabed19b43aade
Miqueas <miqueas2020@yahoo.com>
2023-08-20 10:06:15 -0500

    C: GTK3: refactorized header-bar.c

--- c/gtk3/header-bar.c
+++ c/gtk3/header-bar.c
@@ -1,43 +1,40 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.HeaderBar";
+const gchar *appTitle = "GtkHeaderBar";
+const gchar *appSubtitle = "App subtitle";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.HeaderBar";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win = g_object_new(
-    GTK_TYPE_APPLICATION_WINDOW,
-    "application", self,
-    "default-width", 400,
-    "default-height", 400,
-    NULL
-  );
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *header;
 
-  GtkWidget *header = g_object_new(
-    GTK_TYPE_HEADER_BAR,
-    "visible", TRUE,
-    "show-close-button", TRUE,
-    "title", "My App",
-    "subtitle", "An awesome app that you'll love",
-    NULL
-  );
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  header = gtk_header_bar_new();
 
-  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+  gtk_window_set_titlebar(GTK_WINDOW(window), header);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+
+  gtk_widget_set_visible(header, TRUE);
+  gtk_header_bar_set_title(GTK_HEADER_BAR(header), appTitle);
+  gtk_header_bar_set_subtitle(GTK_HEADER_BAR(header), appSubtitle);
+  gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
 }
