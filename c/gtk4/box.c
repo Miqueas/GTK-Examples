cd61392943d215b11f55d613a5e399a4d808d303
Miqueas <miqueas2020@yahoo.com>
2023-08-20 08:46:02 -0500

    C: GTK4: refactorized box.c

--- c/gtk4/box.c
+++ c/gtk4/box.c
@@ -1,48 +1,52 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
-void btn_clicked(GtkButton *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+void onButtonClicked(GtkButton *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk4.Box";
+const gchar *appTitle = "GtkBox";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk4.Box";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup", G_CALLBACK(app_startup), NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup", G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win, *box, *label, *btn;
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *box, *hintText, *button;
 
-  win = gtk_application_window_new(GTK_APPLICATION(self));
+  window = gtk_application_window_new(GTK_APPLICATION(self));
   box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-  label = gtk_label_new("Click the button");
-  btn = gtk_button_new_with_label("🤔");
+  hintText = gtk_label_new("Click the button");
+  button = gtk_button_new_with_label("🤔");
 
-  g_object_set(box, "halign", GTK_ALIGN_CENTER, "valign", GTK_ALIGN_CENTER, NULL);
-  g_object_set(btn, "halign", GTK_ALIGN_CENTER, "valign", GTK_ALIGN_CENTER, NULL);
+  gtk_window_set_child(GTK_WINDOW(window), box);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  g_signal_connect(btn, "clicked", G_CALLBACK(btn_clicked), NULL);
+  gtk_box_append(GTK_BOX(box), hintText);
+  gtk_box_append(GTK_BOX(box), button);
+  gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
 
-  gtk_box_append(GTK_BOX(box), label);
-  gtk_box_append(GTK_BOX(box), btn);
-
-  gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
-  gtk_window_set_child(GTK_WINDOW(win), box);
+  gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
+  g_signal_connect(button, "clicked", G_CALLBACK(onButtonClicked), NULL);
 }
 
-void btn_clicked(GtkButton *self, gpointer data) {
+void onButtonClicked(GtkButton *self, gpointer data) {
   static int count = 0;
   g_print("You clicked %d times!\n", ++count);
 }
