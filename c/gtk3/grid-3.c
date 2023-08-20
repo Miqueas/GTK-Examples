112c920d129abc0ed6ea5e6b647e2268dbf58342
Miqueas <miqueas2020@yahoo.com>
2023-08-20 10:05:58 -0500

    C: GTK3: refactorized grid-3.c

--- c/gtk3/grid-3.c
+++ c/gtk3/grid-3.c
@@ -1,64 +1,49 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.Grid3";
+const gchar *appTitle = "GtkGrid";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.Grid3";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win, *header, *grid;
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *grid;
 
-  win = g_object_new(
-    GTK_TYPE_APPLICATION_WINDOW,
-    "application", GTK_APPLICATION(self),
-    "default-width", 400,
-    "default-height", 400,
-    NULL
-  );
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  grid = gtk_grid_new();
 
-  header = g_object_new(
-    GTK_TYPE_HEADER_BAR,
-    "visible", TRUE,
-    "show-close-button", TRUE,
-    "title", "GtkGrid",
-    NULL
-  );
+  gtk_container_add(GTK_CONTAINER(window), grid);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  grid = g_object_new(
-    GTK_TYPE_GRID,
-    "visible", TRUE,
-    "column-homogeneous", TRUE,
-    "row-homogeneous", TRUE,
-    "column-spacing", 10,
-    "row-spacing", 10,
-    "halign", GTK_ALIGN_CENTER,
-    "valign", GTK_ALIGN_CENTER,
-    NULL
-  );
-
-  gtk_grid_attach(GTK_GRID(grid), g_object_new(GTK_TYPE_LABEL, "visible", TRUE, "label", "Columns", NULL), 1, 0, 2, 1);
-  gtk_grid_attach(GTK_GRID(grid), g_object_new(GTK_TYPE_LABEL, "visible", TRUE, "label", "Rows", NULL), 0, 0, 1, 3);
-  gtk_grid_attach(GTK_GRID(grid), g_object_new(GTK_TYPE_LABEL, "visible", TRUE, "label", "Top: 1. Left: 1", NULL), 1, 1, 1, 1);
-  gtk_grid_attach(GTK_GRID(grid), g_object_new(GTK_TYPE_LABEL, "visible", TRUE, "label", "Top: 1. Left: 2", NULL), 2, 1, 1, 1);
-  gtk_grid_attach(GTK_GRID(grid), g_object_new(GTK_TYPE_LABEL, "visible", TRUE, "label", "Top: 2. Left: 1", NULL), 1, 2, 1, 1);
-  gtk_grid_attach(GTK_GRID(grid), g_object_new(GTK_TYPE_LABEL, "visible", TRUE, "label", "Top: 2. Left: 2", NULL), 2, 2, 1, 1);
-
-  gtk_container_add(GTK_CONTAINER(win), grid);
-  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+  gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+  gtk_grid_set_row_spacing(GTK_GRID(grid), 10);
+  gtk_grid_set_column_spacing(GTK_GRID(grid), 10);
+  gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+  gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+  gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Columns"), 1, 0, 2, 1);
+  gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Rows"), 0, 0, 1, 3);
+  gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Top: 1. Left: 1"), 1, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Top: 1. Left: 2"), 2, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Top: 2. Left: 1"), 1, 2, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), gtk_label_new("Top: 2. Left: 2"), 2, 2, 1, 1);
+  gtk_widget_show_all(grid);
 }
