61e5e98d908ee36a259da35175800fefbf6c9337
Miqueas <miqueas2020@yahoo.com>
2023-08-20 08:46:40 -0500

    C: GTK4: refactorized label.c

--- c/gtk4/label.c
+++ c/gtk4/label.c
@@ -1,32 +1,36 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk4.Label";
+const gchar *appTitle = "GtkLabel";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk4.Label";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk4.Label";
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup), NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win, *label;
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *label;
 
-  win = gtk_application_window_new(GTK_APPLICATION(self));
+  window = gtk_application_window_new(GTK_APPLICATION(self));
   label = gtk_label_new("Hi there!");
 
-  gtk_window_set_child(GTK_WINDOW(win), label);
-  gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
+  gtk_window_set_child(GTK_WINDOW(window), label);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 }
