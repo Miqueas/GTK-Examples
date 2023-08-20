d38f94d15ee8b022750753901a7c121b9837703b
Miqueas <miqueas2020@yahoo.com>
2023-08-20 10:10:41 -0500

    C: GTK3: refactorized image-2.c

--- c/gtk3/image-2.c
+++ c/gtk3/image-2.c
@@ -1,14 +1,16 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.Image2";
+const gchar *appTitle = "GtkImage";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.Image2";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
   int res = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
@@ -16,38 +18,21 @@ int main(int argc, char **argv) {
   return res;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-
-  GtkWidget *img = g_object_new(
-    GTK_TYPE_IMAGE,
-    "visible", TRUE,
-    "icon-name", "computer",
-    "pixel-size", 256,
-    NULL
-  );
-
-  gtk_container_add(GTK_CONTAINER(win), img);
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win = g_object_new(
-    GTK_TYPE_APPLICATION_WINDOW,
-    "application", self,
-    "default-width", 400,
-    "default-height", 400,
-    NULL
-  );
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *image;
 
-  GtkWidget *header = g_object_new(
-    GTK_TYPE_HEADER_BAR,
-    "visible", TRUE,
-    "show-close-button", TRUE,
-    "title", "My App",
-    "subtitle", "An awesome app that you'll love",
-    NULL
-  );
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  image = gtk_image_new_from_icon_name("computer", GTK_ICON_SIZE_INVALID);
 
-  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+  gtk_container_add(GTK_CONTAINER(window), image);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+
+  gtk_widget_set_visible(image, TRUE);
+  gtk_image_set_pixel_size(GTK_IMAGE(image), 256);
 }
