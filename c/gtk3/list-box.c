0b02898536b9fbfeace5391a50872033c269bc53
Miqueas <miqueas2020@yahoo.com>
2023-08-20 19:59:56 -0500

    C: GTK3: refactorized list-box.c and minor changes

--- c/gtk3/list-box.c
+++ c/gtk3/list-box.c
@@ -1,94 +1,71 @@
 #include <gtk/gtk.h>
 
-void app_activate(GApplication *self, gpointer data);
-void app_startup(GApplication *self, gpointer data);
-void btn_clicked(GtkButton *self, gpointer data);
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+void onButtonClicked(GtkButton *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.ListBox";
+const gchar *appTitle = "GtkListBox";
 
 int main(int argc, char **argv) {
-  const gchar *app_id = "io.github.Miqueas.GTK-Examples.C.Gtk3.ListBox";
-  GtkApplication *app = gtk_application_new(app_id, G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new(appID, G_APPLICATION_DEFAULT_FLAGS);
 
-  g_signal_connect(app, "startup",  G_CALLBACK(app_startup),  NULL);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  g_signal_connect(app, "startup",  G_CALLBACK(onAppStartup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
 
-  int res = g_application_run(G_APPLICATION(app), argc, argv);
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
-  return res;
+  return result;
 }
 
-void app_activate(GApplication *self, gpointer data) {
-  GtkWindow *win = gtk_application_get_active_window(GTK_APPLICATION(self));
-  gtk_window_present(win);
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
 }
 
-void app_startup(GApplication *self, gpointer data) {
-  GtkWidget *win = g_object_new(
-    GTK_TYPE_APPLICATION_WINDOW,
-    "application", GTK_APPLICATION(self),
-    "default-width", 400,
-    "default-height", 400,
-    "border-width", 10,
-    NULL
-  );
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window, *box, *listBox, *scrolledWindow, *button;
 
-  GtkWidget *header = g_object_new(
-    GTK_TYPE_HEADER_BAR,
-    "visible", TRUE,
-    "show-close-button", TRUE,
-    "title", "GtkLabel",
-    NULL
-  );
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  listBox = gtk_list_box_new();
+  scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+  button = gtk_button_new_with_label("Load");
 
-  GtkWidget *box = g_object_new(
-    GTK_TYPE_BOX,
-    "visible", TRUE,
-    "orientation", GTK_ORIENTATION_VERTICAL,
-    "spacing", 10,
-    NULL
-  );
+  gtk_container_add(GTK_CONTAINER(window), box);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  GtkWidget *listbox = g_object_new(
-    GTK_TYPE_LIST_BOX,
-    "visible", TRUE,
-    NULL
-  );
+  gtk_box_pack_start(GTK_BOX(box), scrolledWindow, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
+  gtk_widget_show_all(box);
 
-  GtkWidget *scroll = g_object_new(
-    GTK_TYPE_SCROLLED_WINDOW,
-    "visible", TRUE,
-    "shadow-type", GTK_SHADOW_NONE,
-    "propagate-natural-width", TRUE,
-    "propagate-natural-height", TRUE,
-    NULL
-  );
+  gtk_container_add(GTK_CONTAINER(scrolledWindow), listBox);
+  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolledWindow), GTK_SHADOW_NONE);
+  gtk_scrolled_window_set_propagate_natural_width(GTK_SCROLLED_WINDOW(scrolledWindow), TRUE);
+  gtk_scrolled_window_set_propagate_natural_height(GTK_SCROLLED_WINDOW(scrolledWindow), TRUE);
+  gtk_widget_show_all(scrolledWindow);
 
-  GtkWidget *btn = g_object_new(
-    GTK_TYPE_BUTTON,
-    "visible", TRUE,
-    "label", "Load",
-    "halign", GTK_ALIGN_CENTER,
-    "valign", GTK_ALIGN_CENTER,
-    NULL
-  );
-
-  g_signal_connect(btn, "clicked", G_CALLBACK(btn_clicked), listbox);
-  gtk_container_add(GTK_CONTAINER(scroll), listbox);
-  gtk_box_pack_start(GTK_BOX(box), scroll, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(box), btn, FALSE, FALSE, 0);
-  gtk_container_add(GTK_CONTAINER(win), box);
-  gtk_window_set_titlebar(GTK_WINDOW(win), header);
+  gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
+  gtk_widget_set_margin_start(button, 10);
+  gtk_widget_set_margin_end(button, 10);
+  gtk_widget_set_margin_bottom(button, 10);
+  g_signal_connect(button, "clicked", G_CALLBACK(onButtonClicked), listBox);
 }
 
-void btn_clicked(GtkButton *self, gpointer data) {
+void onButtonClicked(GtkButton *self, gpointer data) {
   for (int i = 0; i < 100; i++) {
     char str[16];
     sprintf(str, "Some text %d", (i + 1));
-    gtk_list_box_insert(GTK_LIST_BOX(data), g_object_new(
-      GTK_TYPE_LABEL,
-      "visible", TRUE,
-      "label", str,
-      NULL
-    ), i);
+    GtkWidget *tempLabel = gtk_label_new(str);
+    gtk_widget_set_margin_start(tempLabel, 10);
+    gtk_widget_set_margin_end(tempLabel, 10);
+    gtk_widget_set_margin_top(tempLabel, 10);
+    gtk_widget_set_margin_bottom(tempLabel, 10);
+    gtk_list_box_insert(GTK_LIST_BOX(data), tempLabel, i);
   }
+
+  gtk_widget_show_all(GTK_WIDGET(data));
 }
