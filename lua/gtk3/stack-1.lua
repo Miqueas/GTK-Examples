cb461ed7eedb67cabd60b7f9822e530ff8f9c84e
Miqueas <miqueas2020@yahoo.com>
2023-09-03 14:23:05 -0500

    Lua: GTK3: refactorized stack-1.lua

--- lua/gtk3/stack-1.lua
+++ lua/gtk3/stack-1.lua
@@ -6,6 +6,9 @@ local Gtk = lgi.require("Gtk", "3.0")
 local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Stack1"
 local appTitle = "GtkStack"
 local app = Gtk.Application({ application_id = appID })
+local labelText = "<span size='xx-large' font_weight='bold'>Page %d</span>"
+local labelTitle = "Page %d"
+local labelName = "page%d"
 
 function app:on_startup()
   local window = Gtk.ApplicationWindow({
@@ -17,65 +20,26 @@ function app:on_startup()
   })
 
   local stack = Gtk.Stack({
-    visible = true,
     transition_type = Gtk.StackTransitionType.SLIDE_LEFT_RIGHT,
     -- Increment this value to see the animation more slow
-    transition_duration = 280,
-
-    {
-      Gtk.Label({
-        visible = true,
-        label = "<span size='xx-large' font_weight='bold'>Page 0</span>",
-        use_markup = true
-      }),
-      title = "Page 0",
-      name = "page0"
-    },
-    {
-      Gtk.Label({
-        visible = true,
-        label = "<span size='xx-large' font_weight='bold'>Page 1</span>",
-        use_markup = true
-      }),
-      title = "Page 1",
-      name = "page1"
-    },
-    {
-      Gtk.Label({
-        visible = true,
-        label = "<span size='xx-large' font_weight='bold'>Page 2</span>",
-        use_markup = true
-      }),
-      title = "Page 2",
-      name = "page2"
-    },
-    {
-      Gtk.Label({
-        visible = true,
-        label = "<span size='xx-large' font_weight='bold'>Page 3</span>",
-        use_markup = true
-      }),
-      title = "Page 3",
-      name = "page3"
-    }
+    transition_duration = 280
   })
+
+  for i = 1, 3 do
+    stack:add_titled(
+      Gtk.Label({ use_markup = true, label = labelText:format(i) }),
+      labelName:format(i),
+      labelTitle:format(i)
+    )
+  end
 
   local box = Gtk.Box({
-    visible = true,
     orientation = Gtk.Orientation.VERTICAL,
-
-    {
-      stack,
-      expand = true
-    },
-
-    Gtk.StackSwitcher({
-      visible = true,
-      stack = stack,
-      halign = Gtk.Align.CENTER
-    })
+    { stack, expand = true },
+    Gtk.StackSwitcher({ stack = stack, halign = Gtk.Align.CENTER })
   })
 
+  box:show_all()
   window:add(box)
 end
 
