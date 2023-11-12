ea0b322a30db335291b059de26ddbd8ee1a1f574
Miqueas <miqueas2020@yahoo.com>
2023-11-12 09:18:37 -0500

    Lua: GTK3: refactorized box

--- lua/gtk3/box.lua
+++ lua/gtk3/box.lua
@@ -3,29 +3,28 @@ local Gtk = lgi.require("Gtk", "3.0")
 
 -- GtkBox: An horizontal or vertical layout container
 
-local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Box1"
-local app_tile = "GtkBox"
-local app = Gtk.Application({ application_id = appID })
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Box"
+local appTitle = "GtkBox"
+local app = Gtk.Application { application_id = appID }
 
 function app:on_startup()
-  local win = Gtk.ApplicationWindow({
-    title = app_tile,
-    application = self,
-    default_width = 400,
-    default_height = 400
-  })
+  local window = Gtk.ApplicationWindow.new(self)
+  local headerBar = Gtk.HeaderBar()
+  local box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 10)
 
-  local box = Gtk.Box({
-    visible = true,
-    orientation = Gtk.Orientation.VERTICAL,
-    spacing = 10,
-    valign = Gtk.Align.CENTER
-  })
+  window:add(box)
+  window:set_titlebar(headerBar)
+  window:set_default_size(400, 400)
 
-  box:pack_start(Gtk.Label({ visible = true, label = "Label 1" }), false, true, 0)
-  box:pack_start(Gtk.Label({ visible = true, label = "Label 2" }), false, true, 0)
+  headerBar.visible = true
+  headerBar.title = appTitle
+  headerBar.show_close_button = true
 
-  win:add(box)
+  box.valign = Gtk.Align.CENTER
+  box.halign = Gtk.Align.CENTER
+  box:pack_start(Gtk.Label.new("A label"), false, true, 0)
+  box:pack_start(Gtk.Button.new_with_label("A button"), false, true, 0)
+  box:show_all()
 end
 
 function app:on_activate()
