0ed4e8adcb97ea7f234bafcb947d4ab255ba4d31
Miqueas <miqueas2020@yahoo.com>
2023-11-27 10:40:16 -0500

    Lua: GTK4: refactorizing header-bar

--- lua/gtk4/header-bar.lua
+++ lua/gtk4/header-bar.lua
@@ -1,38 +1,30 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "4.0")
 
-local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.HeaderBar"
-local appTitle = "<span weight='bold'>GtkHeaderBar</span>"
-local appSubtitle = "A (client-side) window decoration widget"
-local app = Gtk.Application({ application_id = appID })
+local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk4.HeaderBar"
+local appTitle = "GtkHeaderBar"
+local appSubtitle = "App subtitle"
+local app = Gtk.Application { application_id = appID }
 
 function app:on_startup()
-  local window = Gtk.ApplicationWindow({
-    application = self,
-    default_width = 400,
-    default_height = 400
-  })
+  local window = Gtk.ApplicationWindow.new(self)
+  local titleLabel = Gtk.Label.new(appTitle)
+  local subtitleLabel = Gtk.Label.new(appSubtitle)
+  local titleBox = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
+  local headerBar = Gtk.HeaderBar()
 
-  local titleLabel = Gtk.Label({ label = appTitle, use_markup = true })
-  local subtitleLabel = Gtk.Label({ label = appSubtitle })
-  local titleBox = Gtk.Box({
-    orientation = Gtk.Orientation.VERTICAL,
-    margin_top = 4,
-    margin_bottom = 4,
-    valign = Gtk.Align.CENTER
-  })
+  window.titlebar = headerBar
+  window:set_default_size(400, 400)
 
+  titleLabel:add_css_class("title")
+  subtitleLabel:add_css_class("subtitle")
+
+  titleBox.valign = Gtk.Align.CENTER
   titleBox:append(titleLabel)
   titleBox:append(subtitleLabel)
 
-  local header = Gtk.HeaderBar({
-    -- GtkHeaderBar don't sets the window controls buttons by default
-    -- this property changes that behavior
-    show_title_buttons = true,
-    title_widget = titleBox
-  })
-
-  window.titlebar = header
+  headerBar.title_widget = titleBox
+  headerBar.show_title_buttons = true
 end
 
 function app:on_activate()
