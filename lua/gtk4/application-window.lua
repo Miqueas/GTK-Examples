579ea2480baaa3b9ab227accf74df5d9d6e86821
Miqueas <miqueas2020@yahoo.com>
2023-11-27 10:38:22 -0500

    Lua: GTK4: refactorizing application-window

--- lua/gtk4/application-window.lua
+++ lua/gtk4/application-window.lua
@@ -5,22 +5,15 @@ local Gtk = lgi.require("Gtk", "4.0")
 
 local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk4.ApplicationWindow"
 local appTitle = "GtkApplicationWindow"
-local app = Gtk.Application({ application_id = appID })
+local app = Gtk.Application { application_id = appID }
 
 function app:on_startup()
-  -- Since we're not going to use this window, I don't store it in a variable
-  Gtk.ApplicationWindow({
-    title = appTitle,
-    -- By setting the application to the new window is enough to to present it later
-    application = self,
-    default_width = 400,
-    default_height = 400
-  })
+  local window = Gtk.ApplicationWindow.new(self)
+  window.title = appTitle
+  window:set_default_size(400, 400)
 end
 
 function app:on_activate()
-  -- Because we set the application to the window (line 13), now we have a "default/main"
-  -- window to present
   self.active_window:present()
 end
 
