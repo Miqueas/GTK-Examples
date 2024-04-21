67a1258e8d301ee9937b8990da8944d74fc1c85b
Miqueas <miqueas2020@yahoo.com>
2024-04-21 17:03:33 -0500

    Lua: GTK4: small refactor and added builder.lua.in

--- lua/gtk4/box.lua
+++ lua/gtk4/box.lua
@@ -1,11 +1,12 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "4.0")
+local Gio = lgi.Gio
 
 -- GtkBox: An horizontal or vertical layout container
 
 local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk4.Box"
 local app_tile = "GtkBox"
-local app = Gtk.Application { application_id = appID }
+local app = Gtk.Application.new(appID, Gio.ApplicationFlags.FLAGS_NONE)
 
 function app:on_startup()
   local window = Gtk.ApplicationWindow.new(self)
