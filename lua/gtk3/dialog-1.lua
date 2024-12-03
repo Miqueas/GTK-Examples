bcb9747af4c5bec0430e5433f3b5b5c35e35c315
Josué Martínez <JosueTech@proton.me>
2024-12-03 11:46:56 -0500

    Lua: GTK3: updated dialog-1

--- lua/gtk3/dialog-1.lua
+++ lua/gtk3/dialog-1.lua
@@ -1,57 +1,57 @@
 local lgi   = require("lgi")
 local Gtk   = lgi.require("Gtk", "3.0")
-local Pango = lgi.require("Pango", "1.0")
+local Pango = lgi.Pango
+local Gio   = lgi.Gio
 
 -- GtkDialog: A generic popup window
 
 local appID = "io.github.Miqueas.GTK-Examples.Lua.Gtk3.Dialog1"
 local appTitle = "GtkDialog"
-local app = Gtk.Application({ application_id = appID })
+local app = Gtk.Application.new(appID, Gio.ApplicationFlags.DEFAULT_FLAGS)
 
 function app:on_startup()
-  local dialog = Gtk.Dialog({
-    title = appTitle,
-    application = self,
-    default_width = 400,
-    border_width = 10
-  })
+  local dialog = Gtk.Dialog()
+  dialog.title = appTitle
+  dialog.application = self
+  dialog.border_width = 10
+  dialog.default_width = 400
 
   dialog:add_button("Yes 😈👍", Gtk.ResponseType.OK)
   dialog:add_button("No 💀🤚", Gtk.ResponseType.CANCEL)
 
-  local titleText = [[<span size="x-large" font-weight="bold">Universe destruction</span>]]
-  local titleLabel = Gtk.Label({ visible = true, label = titleText, use_markup = true })
+  local titleText = "<span size='x-large' weight='bold'>Universe destruction</span>"
+  local titleLabel = Gtk.Label.new(titleText)
+  titleLabel.use_markup = true
 
   local summaryText = "Our universe has a lot of problems and the only way to fix it is destroying the entire universe and this important decision is now in your hands."
-  local summaryLabel = Gtk.Label({
-    visible = true,
-    label = summaryText,
-    xalign = 0,
-    wrap = true,
-    wrap_mode = Pango.WrapMode.CHAR
-  })
+  local summaryLabel = Gtk.Label.new(summaryText)
+  summaryLabel.wrap = true
+  summaryLabel.halign = 0
+  summaryLabel.wrap_mode = Pango.WrapMode.CHAR
 
-  local epilogText = [[<span size="large" font-weight="bold">Do you accept?</span>]]
-  local epilogLabel = Gtk.Label({ visible = true, label = epilogText, use_markup = true })
+  local epilogText = "<span size='large' weight='bold'>Do you accept?</span>"
+  local epilogLabel = Gtk.Label.new(epilogText)
+  epilogLabel.use_markup = true
 
   local contentBox = dialog:get_content_area()
   contentBox.spacing = 10
   contentBox:pack_start(titleLabel, false, true, 0)
   contentBox:pack_start(summaryLabel, false, true, 0)
   contentBox:pack_start(epilogLabel, false, true, 10)
+  contentBox:show_all()
 end
 
 function app:on_activate()
-  -- When you work with dialogs, use this instead of 'present()'
+  -- When working with dialogs, use this instead of 'present()'
   local response = self.active_window:run()
   self.active_window:destroy()
 
   if response == Gtk.ResponseType.OK then
     print("Universe destroyed! 💥")
   elseif response == Gtk.ResponseType.CANCEL then
-    print("Universe is in peace now! 🙏")
+    print("Universe is in peace now... 🙏")
   else
-    print("Nothing happens! 🤔")
+    print("Nothing happens... 🤔")
   end
 end
 
