0f4740e5ff39458e51a3fe81c1353c0870f12a2e
Josué Martínez <JosueTech@proton.me>
2025-06-22 18:13:24 -0500

    C: GTK3: added 2 new examples (see below):
    - `GtkColorChooserDialog`
    - `GtkColorChooserWidget`

---
+++ c/gtk3/color-chooser-dialog.c
@@ -0,0 +1,49 @@
+#include <gtk/gtk.h>
+
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.ColorChooserDialog";
+const gchar *appTitle = "GtkColorChooserDialog";
+
+int main(int argc, char **argv) {
+  GtkApplication *app = gtk_application_new(appID, 0);
+
+  g_signal_connect(app, "startup", G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
+
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return result;
+}
+
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gint result = gtk_dialog_run(GTK_DIALOG(window));
+
+  switch (result) {
+    case GTK_RESPONSE_ACCEPT:
+    case GTK_RESPONSE_APPLY:
+    case GTK_RESPONSE_YES:
+    case GTK_RESPONSE_OK: {
+      GdkRGBA color;
+      gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(window), &color);
+      g_print("Color: %s\n", gdk_rgba_to_string(&color));
+      break;
+    }
+    default: break;
+  }
+
+  gtk_widget_destroy(GTK_WIDGET(window));
+}
+
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *dialog;
+
+  dialog = gtk_color_chooser_dialog_new("Pick a color", NULL);
+
+  gtk_window_set_title(GTK_WINDOW(dialog), appTitle);
+  gtk_window_set_application(GTK_WINDOW(dialog), GTK_APPLICATION(self));
+  gtk_container_set_border_width(GTK_CONTAINER(dialog), 10);
+}
---
+++ c/gtk3/color-chooser-widget.c
@@ -0,0 +1,84 @@
+#include <gtk/gtk.h>
+
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+void onShowEditorToggled(GtkToggleButton *self, gpointer data);
+void onPrintClicked(GtkButton *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.AppChooserWidget";
+const gchar *appTitle = "GtkAppChooserWidget";
+
+int main(int argc, char **argv) {
+  GtkApplication *app = gtk_application_new(appID, 0);
+
+  g_signal_connect(app, "startup", G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
+
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return result;
+}
+
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
+}
+
+void onAppStartup(GApplication *self, gpointer data) {
+  gboolean showEditor;
+  GtkWidget *window,
+    *box,
+    *colorChooserWidget,
+    *showEditorCheckButton,
+    *printButton;
+
+  window = gtk_application_window_new(GTK_APPLICATION(self));
+  box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  colorChooserWidget = gtk_color_chooser_widget_new();
+  showEditorCheckButton = gtk_check_button_new_with_label("Show editor");
+  printButton = gtk_button_new_with_label("Print Color");
+
+  gtk_container_add(GTK_CONTAINER(window), box);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+  gtk_box_pack_start(GTK_BOX(box), colorChooserWidget, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(box), showEditorCheckButton, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), printButton, FALSE, FALSE, 0);
+  gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
+  gtk_widget_show_all(box);
+
+  g_object_get(G_OBJECT(colorChooserWidget), "show-editor", &showEditor, NULL);
+
+  gtk_toggle_button_set_active(
+    GTK_TOGGLE_BUTTON(showEditorCheckButton),
+    showEditor
+  );
+
+  g_signal_connect(
+    showEditorCheckButton,
+    "toggled",
+    G_CALLBACK(onShowEditorToggled),
+    colorChooserWidget
+  );
+
+  g_signal_connect(
+    printButton,
+    "clicked",
+    G_CALLBACK(onPrintClicked),
+    colorChooserWidget
+  );
+}
+
+void onShowEditorToggled(GtkToggleButton *self, gpointer data) {
+  gboolean showEditor = gtk_toggle_button_get_active(self);
+  g_object_set(G_OBJECT(data), "show-editor", showEditor, NULL);
+}
+
+void onPrintClicked(GtkButton *self, gpointer data) {
+  GdkRGBA color;
+  gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(data), &color);
+  g_print("Color: %s\n", gdk_rgba_to_string(&color));
+}
