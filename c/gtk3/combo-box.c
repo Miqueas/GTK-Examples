846e7a9c358a945084d99c7fb63d569c2876c9e6
Josué Martínez <JosueTech@proton.me>
2025-07-05 09:33:51 -0500

    C: GTK3: minor changes

--- c/gtk3/combo-box.c
+++ c/gtk3/combo-box.c
@@ -37,7 +37,7 @@ void onAppActivate(GApplication *self, gpointer data) {
 void onAppStartup(GApplication *self, gpointer data) {
   GtkWidget *window, *messageLabel, *hintLabel, *comboBox, *box;
   GtkCellRenderer *render;
-  GtkListStore *model;
+  GtkListStore *listStore;
   GtkTreeIter iter;
 
   window = gtk_application_window_new(GTK_APPLICATION(self));
@@ -45,18 +45,18 @@ void onAppStartup(GApplication *self, gpointer data) {
   comboBox = gtk_combo_box_new();
   hintLabel = gtk_label_new("Default option: 0");
   box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-  model = gtk_list_store_new(1, G_TYPE_STRING);
+  listStore = gtk_list_store_new(1, G_TYPE_STRING);
   render = gtk_cell_renderer_text_new();
 
   for (int i = 0; i < 8; i++) {
-    gtk_list_store_append(model, &iter);
-    gtk_list_store_set(model, &iter, 0, items[i], -1);
+    gtk_list_store_append(listStore, &iter);
+    gtk_list_store_set(listStore, &iter, 0, items[i], -1);
   }
 
   gtk_container_add(GTK_CONTAINER(window), box);
   gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  gtk_combo_box_set_model(GTK_COMBO_BOX(comboBox), GTK_TREE_MODEL(model));
+  gtk_combo_box_set_model(GTK_COMBO_BOX(comboBox), GTK_TREE_MODEL(listStore));
   gtk_combo_box_set_active(GTK_COMBO_BOX(comboBox), 0);
   gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(comboBox), render, TRUE);
   gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(comboBox), render, "text", 0, NULL);
