d14be70cb507a09171b50a6c0817bfcb46a67e4b
Josué Martínez <JosueTech@proton.me>
2025-07-21 18:03:23 -0500

    C: GTK3: Added `GtkRevealer` example

---
+++ c/gtk3/revealer.c
@@ -0,0 +1,65 @@
+#include <gtk/gtk.h>
+
+// `GtkRevealer` is a container which animates the transition of its child from
+// invisible to visible.
+
+void onAppActivate(GApplication *self, gpointer data);
+void onAppStartup(GApplication *self, gpointer data);
+void onClicked(GtkButton *self, gpointer data);
+
+const gchar *appID = "io.github.Miqueas.GTK-Examples.C.Gtk3.Revealer";
+const gchar *appTitle = "GtkRevealer";
+
+int main(int argc, char **argv) {
+  GtkApplication *app = gtk_application_new(appID, 0);
+  g_signal_connect(app, "startup", G_CALLBACK(onAppStartup), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(onAppActivate), NULL);
+
+  int result = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return result;
+}
+
+void onAppActivate(GApplication *self, gpointer data) {
+  GtkWindow *window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  gtk_window_present(window);
+}
+
+void onAppStartup(GApplication *self, gpointer data) {
+  GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(self));
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  GtkWidget *revealer = gtk_revealer_new();
+  GtkWidget *button = gtk_button_new_with_label("Toggle Revealer");
+  GtkWidget *label = gtk_label_new("Revealed Content");
+
+  gtk_container_add(GTK_CONTAINER(window), box);
+  gtk_window_set_title(GTK_WINDOW(window), appTitle);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+  gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), revealer, TRUE, TRUE, 0);
+  gtk_widget_show_all(box);
+  gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
+
+  gtk_container_add(GTK_CONTAINER(revealer), label);
+  gtk_revealer_set_transition_type(
+    GTK_REVEALER(revealer),
+    GTK_REVEALER_TRANSITION_TYPE_CROSSFADE
+  );
+  gtk_revealer_set_transition_duration(GTK_REVEALER(revealer), 150);
+
+  g_signal_connect(button, "clicked", G_CALLBACK(onClicked), revealer);
+
+  gtk_widget_show(label);
+}
+
+void onClicked(GtkButton *self, gpointer data) {
+  GtkRevealer *revealer = GTK_REVEALER(data);
+  gtk_revealer_set_reveal_child(
+    revealer,
+    !gtk_revealer_get_reveal_child(revealer)
+  );
+}
