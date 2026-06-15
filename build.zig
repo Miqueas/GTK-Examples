32c968a3916d1d249c93d28d86bad62d475bc5e8
Josué Martínez <JosueTech@proton.me>
2026-06-15 18:13:34 -0500

    Zig: Added root build.zig

---
+++ build.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // Generates bindings first
+    const codegen = b.addSystemCommand(&.{
+        b.graph.zig_exe,
+        "build", "codegen",
+        "-Dmodules=Gtk-4.0",
+        "-Dmodules=Gtk-3.0",
+        "-Dmodules=Gio-2.0",
+        "-Dmodules=GLib-2.0",
+        "-Dmodules=GObject-2.0",
+    });
+    codegen.setCwd(b.path("zig-gobject"));
+
+    // Then builds the examples
+    const project = b.addSystemCommand(&.{
+        b.graph.zig_exe,
+        "build",
+    });
+    project.setCwd(b.path("zig"));
+    project.step.dependOn(&codegen.step);
+
+    // Ensures previous steps are properly executed when running `zig build`
+    b.getInstallStep().dependOn(&project.step);
+}
