f874b15e1857ff0f0e511a3e3355b682039a6c9f
Josué Martínez <JosueTech@proton.me>
2026-07-01 23:50:14 -0500

    C: GTK4: No longer using .in files

---
+++ c/gtk4/image-1.c
@@ -0,0 +1,39 @@
+#include <gtk/gtk.h>
+
+static void on_app_activate(GApplication* self, gpointer data);
+static void on_app_startup(GApplication* self, gpointer data);
+
+const static gchar* APP_ID = "io.github.Miqueas.GTK-Examples.C.Gtk3.Image1";
+const static gchar* APP_TITLE = "GtkImage";
+
+gint main(gint argc, gchar** argv) {
+  GtkApplication* app = gtk_application_new(APP_ID, 0);
+
+  g_signal_connect(app, "startup",  G_CALLBACK(on_app_startup),  NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(on_app_activate), NULL);
+
+  gint result = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return result;
+}
+
+static void on_app_activate(GApplication* self, gpointer data) {
+  GtkWindow* window = gtk_application_get_active_window(GTK_APPLICATION(self));
+  if (window != NULL) gtk_window_present(window);
+}
+
+static void on_app_startup(GApplication* self, gpointer data) {
+  gchar* src_path = g_path_get_dirname(__FILE__);
+  gchar* image_path = g_build_filename(src_path, "../../data/", "grid-explained.png", NULL);
+
+  GtkWidget* window = gtk_application_window_new(GTK_APPLICATION(self));
+  GtkWidget* image = gtk_image_new_from_file("@imagePath@");
+
+  gtk_window_set_child(GTK_WINDOW(window), image);
+  gtk_window_set_title(GTK_WINDOW(window), APP_TITLE);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+
+  g_free(src_path);
+  g_free(image_path);
+}
