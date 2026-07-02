6d834695ecd6d51258487097678a6d8006c8a5e5
Josué Martínez <JosueTech@proton.me>
2026-07-02 10:09:56 -0500

    C: GTK3: Minor changes

--- c/gtk3/file-chooser-widget.c
+++ c/gtk3/file-chooser-widget.c
@@ -2,9 +2,9 @@
 
 static void on_app_activate(GApplication* self, gpointer data);
 static void on_app_startup(GApplication* self, gpointer data);
-void onSelectionChanged(GtkFileChooser* self, gpointer data);
-void onShowHidden(GtkFileChooserWidget* self, gpointer data);
-void onFileActivated(GtkFileChooser* self, gpointer data);
+static void on_selection_changed(GtkFileChooser* self, gpointer data);
+static void on_show_hidden(GtkFileChooserWidget* self, gpointer data);
+static void on_file_activated(GtkFileChooser* self, gpointer data);
 
 const static gchar* APP_ID = "io.github.Miqueas.GTK-Examples.C.Gtk3.FileChooserWidget";
 const static gchar* APP_TITLE = "GtkFileChooserWidget";
@@ -27,56 +27,34 @@ static void on_app_activate(GApplication* self, gpointer data) {
 
 static void on_app_startup(GApplication* self, gpointer data) {
   GtkWidget* window = gtk_application_window_new(GTK_APPLICATION(self));
-  GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
   GtkWidget* widget = gtk_file_chooser_widget_new(GTK_FILE_CHOOSER_ACTION_OPEN);
 
-  gtk_container_add(GTK_CONTAINER(window), box);
+  gtk_container_add(GTK_CONTAINER(window), widget);
   gtk_window_set_title(GTK_WINDOW(window), APP_TITLE);
   gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-  gtk_box_pack_start(GTK_BOX(box), widget, TRUE, TRUE, 0);
-  gtk_widget_show_all(box);
-
-  g_signal_connect(widget, "show-hidden", G_CALLBACK(onShowHidden), NULL);
-  g_signal_connect(
-    widget,
-    "file-activated",
-    G_CALLBACK(onFileActivated),
-    window
-  );
-  g_signal_connect(
-    widget,
-    "selection-changed",
-    G_CALLBACK(onSelectionChanged),
-    NULL
-  );
+  gtk_widget_show(widget);
+  g_signal_connect(widget, "show-hidden", G_CALLBACK(on_show_hidden), NULL);
+  g_signal_connect(widget, "file-activated", G_CALLBACK(on_file_activated), window);
+  g_signal_connect(widget, "selection-changed", G_CALLBACK(on_selection_changed), NULL);
 }
 
-void onShowHidden(GtkFileChooserWidget* self, gpointer data) {
+void on_show_hidden(GtkFileChooserWidget* self, gpointer data) {
   gboolean visible = gtk_file_chooser_get_show_hidden(GTK_FILE_CHOOSER(self));
   g_print("Show hidden files: %s\n", visible ? "Yes" : "No");
 }
 
-void onFileActivated(GtkFileChooser* self, gpointer data) {
+void on_file_activated(GtkFileChooser* self, gpointer data) {
   gchar* filename = gtk_file_chooser_get_filename(self);
-  if (filename == NULL) {
-    g_free(filename);
-    return;
-  }
-
+  if (filename == NULL) return;
   g_print("File chosen: %s\n", filename);
   g_free(filename);
   gtk_widget_destroy(GTK_WIDGET(data));
 }
 
-void onSelectionChanged(GtkFileChooser* self, gpointer data) {
+void on_selection_changed(GtkFileChooser* self, gpointer data) {
   gchar* filename = gtk_file_chooser_get_filename(self);
-
-  if (filename == NULL) {
-    g_free(filename);
-    return;
-  }
-
+  if (filename == NULL) return;
   g_print("File selected: %s\n", filename);
   g_free(filename);
 }
